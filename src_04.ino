ffe08186ca0ed6854717ea77542bf14f9ea99a79
haider8645 <fauji1991@gmail.com>
2019-03-29 18:43:37 +0100

    Made code more generic and refactoring applied.

--- src_04.ino
+++ src_04.ino
@@ -1,30 +1,33 @@
 #include <state_machine.h>
-int TRIG_PIN_LEAVE = 26;
-int TRIG_PIN_ARRIVE = 14;
-int ECHO_PIN_LEAVE = 27;
-int ECHO_PIN_ARRIVE = 25;
-int LED_INDICATOR_CAR_DETECTION = 13;
 
-const unsigned int MAX_DIST = 23323;
+#define LED_INDICATOR_CAR_DETECTION   13
+#define TRIG_PIN_ARRIVE               14
+#define ECHO_PIN_ARRIVE               25
+#define TRIG_PIN_LEAVE                26
+#define ECHO_PIN_LEAVE                27
+
+#define DETECTION_UPPER_THRESHOLD 0.5 // in meters
+#define DETECTION_LOWER_THRESHOLD 0.2  
+ 
+//const unsigned int MAX_DIST = 23323;
 
 StateMachine sm;
 StateMachine::Event sensor_arrive_event = StateMachine::Event::UNDEFINED;
-StateMachine::Event sensor_leave_event = StateMachine::Event::UNDEFINED;
+StateMachine::Event sensor_leave_event  = StateMachine::Event::UNDEFINED;
 
-int sensor_arrive_last_state = 0;
-int sensor_leave_last_state = 0;
-
-int sensor_arrive_current_state  = 0;
-int sensor_leave_current_state  = 0;
+bool sensor_arrive_last_state     = LOW;
+bool sensor_arrive_current_state  = LOW;
+bool sensor_leave_last_state      = LOW;
+bool sensor_leave_current_state   = LOW;
 
 void setup()
 {
     pinMode(TRIG_PIN_ARRIVE, OUTPUT);
-    pinMode(TRIG_PIN_LEAVE, OUTPUT);
-    pinMode(ECHO_PIN_ARRIVE, LOW);
-    pinMode(ECHO_PIN_LEAVE,LOW);
+    pinMode(TRIG_PIN_LEAVE,  OUTPUT);
+    pinMode(ECHO_PIN_ARRIVE,    LOW);
+    pinMode(ECHO_PIN_LEAVE,     LOW);
     pinMode(LED_INDICATOR_CAR_DETECTION,OUTPUT);
-	  Serial.begin(115200);
+	Serial.begin(115200);
     sm.Start();
 }
 
@@ -38,15 +41,17 @@ float getDistance(int pin_trig, int pin_echo)
   unsigned long t2;
   unsigned long pulse_width;
   float distance = 0.f;
+
   while ( digitalRead(pin_echo) == 0){}
   // Measure how long the echo pin was held high (pulse width)
   // Note: the micros() counter will overflow after ~70 min
   t1 = micros();
+  
   while ( digitalRead(pin_echo) == 1);
   t2 = micros();
   // Wait for pulse on echo pin
   pulse_width = t2 - t1;
-
+  
   // Calculate distance in centimeters and meters. The constants
   // are found in the datasheet, and calculated from the assumed speed 
   //of sound in air at sea level (~340 m/s).
@@ -61,7 +66,7 @@ void loop()
   distance_arrive = getDistance(TRIG_PIN_ARRIVE,ECHO_PIN_ARRIVE);
   distance_leave  = getDistance(TRIG_PIN_LEAVE,ECHO_PIN_LEAVE);
 
-  if (distance_arrive >= 0.2 && distance_arrive <= 0.5)
+   if (distance_arrive >= DETECTION_LOWER_THRESHOLD && distance_arrive <= DETECTION_UPPER_THRESHOLD)
    {
        sensor_arrive_current_state = HIGH;
    }
@@ -70,7 +75,7 @@ void loop()
        sensor_arrive_current_state = LOW;
    }
    
-   if (distance_leave >= 0.2 && distance_leave <= 0.5)
+   if (distance_leave >= DETECTION_LOWER_THRESHOLD && distance_leave <= DETECTION_UPPER_THRESHOLD)
    {
        sensor_leave_current_state = HIGH;
    }
@@ -90,11 +95,15 @@ void loop()
         sensor_arrive_event = StateMachine::Event::SENSOR_LOW_TO_HIGH;
     else if (sensor_arrive_last_state == HIGH && sensor_arrive_current_state == LOW)
         sensor_arrive_event = StateMachine::Event::SENSOR_HIGH_TO_LOW;
+    else
+        sensor_arrive_event = StateMachine::Event::SENSOR_NO_CHANGE;
 
     if (sensor_leave_last_state == LOW && sensor_leave_current_state == HIGH)
         sensor_leave_event = StateMachine::Event::SENSOR_LOW_TO_HIGH;
     else if (sensor_leave_last_state == HIGH && sensor_leave_current_state == LOW)
         sensor_leave_event = StateMachine::Event::SENSOR_HIGH_TO_LOW;
+    else
+        sensor_leave_event = StateMachine::Event::SENSOR_NO_CHANGE;    
 
     sm.SetEvent(sensor_arrive_event,sensor_leave_event);
     sm.SetSensorState(static_cast<StateMachine::SensorState>(sensor_arrive_current_state),static_cast<StateMachine::SensorState>(sensor_leave_current_state));
